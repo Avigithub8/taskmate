300d20f10b7e74ce76fd8eefca2f62c7a75a5a42
Avigithub8 <sharma.avi844@gmail.com>
2020-07-28 00:27:29 +0530

    Uploading Taskmate Project

---
+++ todolist_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from todolist_app.models import Tasklist
+# Register your models here.
+
+admin.site.register(Tasklist)
+
+
---
+++ users_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+#from django.http import HttpResponse
+from .forms import CustomRegisterForm
+from django.contrib import messages
+from django.shortcuts import redirect
+
+# Create your views here.
+
+def register(request):
+    #return HttpResponse("users_app working!")
+    if request.method=="POST":
+          register_form = CustomRegisterForm(request.POST)
+          if register_form.is_valid():
+              register_form.save()
+              messages.success(request , ("New User Account Created , Login To Get Started"))
+              return redirect('register')
+    else:
+        register_form = CustomRegisterForm()
+    return render(request, 'register.html', {'register_form': register_form})
+
